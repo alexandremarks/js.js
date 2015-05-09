32061a46dca70746cf23fde7d8885420c3bfda5c
alexandremarks <amarksweb@gmail.com>
2015-05-09 10:32:32 -0300

    Js Básico

---
+++ js.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$( document ).ready(function() {
+    $(".shopping").css({ opacity: 0.5 });
+	
+	$('.centro').click(function (e) {
+			$('.centro').css({ opacity: 1 });
+			$('.shopping').css({ opacity: 0.5 });
+            $('.unidade2').hide();
+			$('.unidade1').fadeIn();
+        });
+		
+	$('.shopping').click(function (e) {
+			$('.shopping').css({ opacity: 1 });
+			$('.centro').css({ opacity: 0.5 });
+            $('.unidade1').hide();
+			$('.unidade2').fadeIn();
+        });
+});
